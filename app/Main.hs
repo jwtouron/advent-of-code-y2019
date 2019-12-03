b01899dcd6bf2b0fc1522f6fbefd202dc55d08f0
John Touron <john.touron@root9b.com>
2019-12-03 10:25:35 -0600

    Day 3

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Lib.Day1 as Day1
 import Lib.Day2 as Day2
+import Lib.Day3 as Day3
 import System.Environment
 
 main :: IO ()
@@ -10,3 +11,4 @@ main = do
   case day of
     1 -> Day1.solve
     2 -> Day2.solve
+    3 -> Day3.solve
---
+++ src/Lib/Day3.hs
@@ -0,0 +1,106 @@
+{-# LANGUAGE RecordWildCards #-}
+module Lib.Day3
+  ( solve
+  )
+where
+
+import           Control.Arrow
+import           Control.Monad
+import           Data.List                      ( foldl'
+                                                , minimumBy
+                                                )
+import           Data.List.Split                ( splitOn )
+import           Data.Map.Strict                ( Map )
+import qualified Data.Map.Strict               as Map
+import           Data.Set                       ( Set )
+import qualified Data.Set                      as Set
+import           Data.Ord                       ( comparing )
+import           Lib.Util
+
+listToTuple2 :: [a] -> (a, a)
+listToTuple2 [x1, x2] = (x1, x2)
+listToTuple2 _        = error "listtotuple2: wrong list size"
+
+data Direction
+  = U
+  | D
+  | L
+  | R
+  deriving (Show)
+
+data Instruction =
+  Instr Direction Int
+  deriving (Show)
+
+parseInstruction :: String -> Instruction
+parseInstruction (c : cs) =
+  let dir = case c of
+        'U' -> U
+        'D' -> D
+        'R' -> R
+        'L' -> L
+        _   -> error $ "Unknown character: " ++ [c]
+  in  Instr dir (read cs)
+
+parseInstruction [] = error "Empty instruction"
+
+input :: IO ([Instruction], [Instruction])
+input =
+  listToTuple2 . map (map parseInstruction . splitOn ",") . lines <$> readFile "input/day3.txt"
+
+example1 :: ([Instruction], [Instruction])
+example1 = join (***) (map parseInstruction . splitOn ",") ("R8,U5,L5,D3", "U7,R6,D4,L4")
+
+type Point = (Int, Int)
+
+data Path = Path { pathHead :: Point, pathLength :: Int, pathPoints :: Map Point Int } deriving (Show)
+
+newPath :: Path
+newPath = Path (0, 0) 0 (Map.singleton (0, 0) 0)
+
+manhattenDistance :: Point -> Point -> Int
+manhattenDistance (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
+
+followInstruction :: Instruction -> Path -> Path
+followInstruction (Instr dir dist) path@Path {..} = foldl' f path points
+ where
+  f Path {..} point =
+    let pathLength' = pathLength + 1
+    in  Path { pathHead   = point
+             , pathLength = pathLength'
+             , pathPoints = Map.insert point pathLength' pathPoints
+             }
+  (x, y) = pathHead
+  points = case dir of
+    U -> [ (x, y') | y' <- [y + 1 .. y + dist] ]
+    D -> [ (x, y') | y' <- [y - 1, y - 2 .. y - dist] ]
+    R -> [ (x', y) | x' <- [x + 1 .. x + dist] ]
+    L -> [ (x', y) | x' <- [x - 1, x - 2 .. x - dist] ]
+
+followInstructions :: [Instruction] -> Path -> Path
+followInstructions is path = foldl' (flip followInstruction) path is
+
+pathIntersections :: Path -> Path -> Map Point Int
+pathIntersections path1 path2 =
+  Map.delete (0, 0)
+    . uncurry (Map.intersectionWith (+))
+    . listToTuple2
+    . map pathPoints
+    $ [path1, path2]
+
+instructionIntersections :: [Instruction] -> [Instruction] -> Map Point Int
+instructionIntersections is1 is2 =
+  uncurry pathIntersections . listToTuple2 . map (`followInstructions` newPath) $ [is1, is2]
+
+solvePart1 :: ([Instruction], [Instruction]) -> Int
+solvePart1 iss = minimum . map (manhattenDistance (0, 0)) . Set.elems $ intersections
+ where
+  intersections :: Set Point
+  intersections = Map.keysSet $ uncurry instructionIntersections iss
+
+solvePart2 :: ([Instruction], [Instruction]) -> Int
+solvePart2 iss = snd $ minimumBy (comparing snd) $ Map.assocs intersections
+  where intersections = uncurry instructionIntersections iss
+
+solve :: IO ()
+solve = input >>= mapM_ print . sequence [assert' 293 . solvePart1, assert' 27306 . solvePart2]
